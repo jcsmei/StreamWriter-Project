a90e955a1f0b4147d8ba66fe972ebbffa7fdb59c
jcsme <jcsme@DESKTOP-O6725QL>
2017-02-12 13:54:22 -0500

    Add project files.

---
+++ StreamWriter Project/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace StreamWriter_Project
+{
+    partial class frmStreamWriterProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAttendance = new System.Windows.Forms.Button();
+            this.btnHelloWorld = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnNumberList = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAttendance
+            // 
+            this.btnAttendance.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAttendance.Location = new System.Drawing.Point(18, 111);
+            this.btnAttendance.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAttendance.Name = "btnAttendance";
+            this.btnAttendance.Size = new System.Drawing.Size(150, 50);
+            this.btnAttendance.TabIndex = 0;
+            this.btnAttendance.Text = "Take Attendance";
+            this.btnAttendance.UseVisualStyleBackColor = true;
+            this.btnAttendance.Click += new System.EventHandler(this.btnAttendance_Click);
+            // 
+            // btnHelloWorld
+            // 
+            this.btnHelloWorld.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelloWorld.Location = new System.Drawing.Point(186, 111);
+            this.btnHelloWorld.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnHelloWorld.Name = "btnHelloWorld";
+            this.btnHelloWorld.Size = new System.Drawing.Size(150, 50);
+            this.btnHelloWorld.TabIndex = 1;
+            this.btnHelloWorld.Text = "Hello World";
+            this.btnHelloWorld.UseVisualStyleBackColor = true;
+            this.btnHelloWorld.Click += new System.EventHandler(this.btnHelloWorld_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(18, 190);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Finished";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnNumberList
+            // 
+            this.btnNumberList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNumberList.Location = new System.Drawing.Point(186, 190);
+            this.btnNumberList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnNumberList.Name = "btnNumberList";
+            this.btnNumberList.Size = new System.Drawing.Size(150, 50);
+            this.btnNumberList.TabIndex = 3;
+            this.btnNumberList.Text = "Number List";
+            this.btnNumberList.UseVisualStyleBackColor = true;
+            this.btnNumberList.Click += new System.EventHandler(this.btnNumberList_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Your Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(18, 66);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(318, 22);
+            this.txtName.TabIndex = 5;
+            // 
+            // frmStreamWriterProject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 271);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNumberList);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnHelloWorld);
+            this.Controls.Add(this.btnAttendance);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmStreamWriterProject";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAttendance;
+        private System.Windows.Forms.Button btnHelloWorld;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnNumberList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ StreamWriter Project/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace StreamWriter_Project
+{
+    public partial class frmStreamWriterProject : Form
+    {
+        public frmStreamWriterProject()
+        {
+            InitializeComponent();
+        }
+      
+        StreamWriter OutFile = new StreamWriter(@"U:\Data\Attendance.txt", true);
+              
+        private void btnAttendance_Click(object sender, EventArgs e)
+        {
+            if (OutFile == null || OutFile.BaseStream == null)
+            {
+                OutFile = File.AppendText(@"U:\Data\Attendance.txt");
+            }
+            try
+            {
+                OutFile.WriteLine(txtName.Text);
+                MessageBox.Show(txtName.Text + " is on the attendance list now.");
+                txtName.Focus();
+                txtName.SelectAll();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string path = @"U:\Data\Attendance02.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path).Dispose();
+                using (StreamWriter OutFile = new StreamWriter(path, true))
+                {
+                    try
+                    {
+                        OutFile.WriteLine(txtName.Text);
+                        MessageBox.Show(txtName.Text + " is on the attendance list now.");
+                        txtName.Focus();
+                        txtName.SelectAll();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            else if (File.Exists(path))
+            {
+                using (StreamWriter OutFile = new StreamWriter(path))
+                {
+                    MessageBox.Show("File already exits.");
+                    
+                }
+            }
+        }
+        
+            
+        private void btnHelloWorld_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter outHelloFile;
+                outHelloFile = File.AppendText(@"U:\Data\HelloWorld.txt");
+                outHelloFile.Write("Hello World! ");
+                outHelloFile.Close();
+                MessageBox.Show(@"The File U:\Data\HelloWorld.txt is saved");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnNumberList_Click(object sender, EventArgs e)
+        {
+            StreamWriter numberList;
+            numberList = File.CreateText(@"U:\data\numberlist.txt");
+            for (int x = 0; x <= 10000; x += 100)
+            {
+                numberList.WriteLine(x.ToString());
+                numberList.Write(x.ToString() + ", ");
+                //put numbers on one line but with commas and a space
+            }
+            numberList.Close();
+            MessageBox.Show("Text File numberlist.txt is created.");
+        }
+    }
+}
+ 
